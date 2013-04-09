9fbcb1ad2e1fbd366b60caebfe18f56f63afd1b5
micah <michaeleschbacher.com>
2013-04-09 14:52:48 -0500

    Added tests for setting the size

--- tests/GravatarTest.php
+++ tests/GravatarTest.php
@@ -66,6 +66,34 @@ class GravatarTest extends PHPUnit_Framework_TestCase
 			$this->fail('Should have thrown an exception');
 		}
 	}
+
+	public function testSetSizeValidValue()
+	{
+		$inputs = array(
+			1, 45, 2048
+		);
+
+		$gravatar = new MockGravatar();
+		foreach ($inputs as $input) {
+			$gravatar->setSize($input);
+			$this->assertEquals($input, $gravatar->size);
+		}
+	}
+	public function testSetSizeInvalidValues()
+	{
+		$inputs = array(
+			'asdf', -5, 2041
+		);
+		$gravatar = new Gravatar();
+		foreach ($inputs as $input) {
+			try {
+				$gravatar->setSize($input);
+				$this->fail('Should have thrown an exception');
+			} catch(Exception $e) {
+				continue;
+			}
+		}
+	}
 }
 
 class MockGravatar extends Gravatar
