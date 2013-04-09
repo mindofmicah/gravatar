10d6f8e22bf58fab34e90e3ddfe861d28815bee2
micah <michaeleschbacher.com>
2013-04-09 13:42:16 -0500

    Added some tests for setting the rating

--- tests/GravatarTest.php
+++ tests/GravatarTest.php
@@ -30,4 +30,52 @@ class GravatarTest extends PHPUnit_Framework_TestCase
 			$this->assertEquals($expected_src, Gravatar::quickProcess($input));
 		}
 	}
+
+	public function testSetRatingWithValidValue()
+	{
+		$inputs = array(
+			'g'=>'g',
+			Gravatar::RATING_G =>'g',
+			'pg'=>'pg',
+			'r'=>'r',
+			'R'=>'r',
+			Gravatar::RATING_PG => 'pg',
+			'X'=>'x'
+		);
+	
+		$gravatar = new MockGravatar;
+		foreach ($inputs as $input => $expected) {
+			$gravatar->setRating($input);
+			$this->assertEquals($expected, $gravatar->rating);
+		}
+	}
+
+	public function testSetRatingWithInvalidValue()
+	{
+		$invalids = array(
+			'gh', 'taco', 'm', 'f'
+		);
+
+		$gravatar = new Gravatar;
+		foreach ($invalids as $input) {
+			try {
+				$gravatar->setRating($input);
+			} catch (Exception $e) {
+				continue;
+			}
+			$this->fail('Should have thrown an exception');
+		}
+	}
+}
+
+class MockGravatar extends Gravatar
+{
+	public function __get($param)
+	{
+		return $this->$param;
+	}
+	public function __set($param, $value)
+	{
+		$this->$param = $value;
+	}
 }
